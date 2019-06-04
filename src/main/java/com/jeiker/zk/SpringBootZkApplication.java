32ae5d3382e08759ba669c0fbb3ed37b9461e8d7
jeikerxiao <jeiker@126.com>
2019-06-04 11:58:35 +0800

    init project
    init project

---
+++ src/main/java/com/jeiker/zk/SpringBootZkApplication.java
@@ -0,0 +1,13 @@
+package com.jeiker.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZkApplication.class, args);
+	}
+
+}
